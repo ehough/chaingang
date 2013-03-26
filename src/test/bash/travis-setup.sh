3de214b1ecc137b3a3792cf6156ea0aaf6f1c46d
Eric Hough <eric@ehough.com>
2013-03-26 19:22:30 -0400

    Forgot to include travis-setup.sh

---
+++ src/test/bash/travis-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $TRAVIS_BUILD_DIR
+
+wget https://raw.github.com/ehough/throwback/develop/throwback.php
+
+php throwback.php
---
+++ src/test/php/throwback.php
@@ -0,0 +1,9 @@
+<?php
+
+__throwback::$config = array(
+
+    'name'         => 'ehough_chaingang',
+    'autoload'     => dirname(__FILE__) . '/../../main/php',
+    'dependencies' => array(
+    )
+);
