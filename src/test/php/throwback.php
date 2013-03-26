2653fb7c14c18f850e967157280dc41882c7cc59
Eric Hough <eric@ehough.com>
2013-03-26 19:57:11 -0400

    Including Mockery in throwback build

--- src/test/php/throwback.php
+++ src/test/php/throwback.php
@@ -5,5 +5,7 @@ __throwback::$config = array(
     'name'         => 'ehough_chaingang',
     'autoload'     => dirname(__FILE__) . '/../../main/php',
     'dependencies' => array(
+
+        'mockery/mockery' => 'https://github.com/padraic/mockery.git'
     )
 );
